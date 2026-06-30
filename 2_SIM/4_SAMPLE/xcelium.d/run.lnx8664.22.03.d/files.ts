6542952dc70b61c71e74ca32386461201ac8e6a4
ManHyuk <gksaksgur18@gmail.com>
2026-07-01 02:42:17 +0900

    Update SoC design and simulation records

---
+++ 2_SIM/4_SAMPLE/xcelium.d/run.lnx8664.22.03.d/files.ts
@@ -0,0 +1,3 @@
+1780574706 /home/hah004/cds.lib
+1782811008 /home/hah004/Basic_SoC-_Implementation/2_SIM/4_SAMPLE/pipeline.v
+1782811090 /home/hah004/Basic_SoC-_Implementation/2_SIM/4_SAMPLE/tb_pipeline.v
