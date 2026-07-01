d20507f84030b2250503ce6ba696c264fce889bc
ManHyuk <gksaksgur18@gmail.com>
2026-07-01 22:08:43 +0900

    Update SoC design and simulation records

---
+++ 1_RTL/4_COMB_LOGIC/4_COMPARATOR/comparator.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+module comparator(
+	input [3:0] i_a	,	
+	input [3:0] i_b	,
+	output o_equal	,
+	output o_greater,
+	output o_less	
+);
+
+assign o_equal	  = (i_a == i_b);
+assign o_greater  = (i_a > i_b)	;
+assign o_less	  = (i_a < i_b)	;
+
+endmodule
